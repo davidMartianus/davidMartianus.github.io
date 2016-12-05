f10dea30b6b36f4f9159fdf802a9d3661b88c900
davidMartianus <davidmartianus.simarmata@gmail.com>
2016-12-05 19:31:06 +0700

    Commit 5 Desember 2016

--- js/main.js
+++ js/main.js
@@ -26,3 +26,35 @@ $(function(){
 		scroll=moved;
 	});
 });
+
+$(function(){
+	var total = $('.imageport-wrap .imageport').length;
+
+	$('.fa-chevron-right').click(function(){
+		var ind;
+		$('.imageport-wrap').find('.imageport').map(function(){
+			if ($(this).css('display') == 'block')
+				ind = $(this).index();
+		});
+
+		if (ind == total-1)
+			ind= -1;
+
+		$('.imageport-wrap').find('.imageport').eq(ind).toggle("slide", { direction: "left"}, 200);
+		$('.imageport-wrap').find('.imageport').eq(ind+1).toggle("slide", { direction: "right"}, 400);
+	});
+
+	$('.fa-chevron-left').click(function(){
+		var ind;
+		$('.imageport-wrap').find('.imageport').map(function(){
+			if ($(this).css('display') == 'block')
+				ind = $(this).index();
+		});
+
+		if (ind == -1)
+			ind = total;
+
+		$('.imageport-wrap').find('.imageport').eq(ind).toggle("slide", { direction: "right"}, 200);
+		$('.imageport-wrap').find('.imageport').eq(ind-1).toggle("slide", { direction: "left"}, 400);
+	});
+})
